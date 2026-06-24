0283d7a118aedff967f21f38ca7c6bbaf1def408
vijayv511 <88069606+vijayv511@users.noreply.github.com>
2026-06-24 16:19:17 +0530

    Update MVCConfig.java

--- src/main/java/com/work/thymeleaf/config/MVCConfig.java
+++ src/main/java/com/work/thymeleaf/config/MVCConfig.java
@@ -6,10 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MVCConfig  implements WebMvcConfigurer {
 	
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("/");
-		registry.addViewController("/").setViewName("/");
+		registry.addViewController("/home").setViewName("/home");
+		registry.addViewController("/home").setViewName("/home");
 		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/").setViewName("/");
+		registry.addViewController("/home").setViewName("/home");
 	}
 
 }
